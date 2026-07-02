4b00a9042445daef2147710b9d5459703db64888
Mohammad Danish <danishsince04@gmail.com>
2026-07-02 22:42:24 +0530

    postgres configured

--- backend/src/main/java/com/danish/backend/controller/HealthController.java
+++ backend/src/main/java/com/danish/backend/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.danish.backend;
+package com.danish.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
